3b462d769196798bb1b2c72bafcf2af342cafdfd
siddharth_dumbre <dumbre.si@northeastern.edu>
2024-10-07 19:07:22 -0400

    Added vpc, subnet and route tables

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,10 +1,65 @@
-
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "main-vpc"
   }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnets_cidrs, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnets_cidrs, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-gateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets_cidrs)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  # No internet access for private subnets
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets_cidrs)
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
+  route_table_id = aws_route_table.private.id
 }
--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -5,3 +5,18 @@ variable "aws_region" {
 variable "aws_profile" {
   type = string
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets_cidrs" {
+  description = "CIDR for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
