0534ffebc875cbcdf09294cd2c385e6181c3e258
siddharth_dumbre <dumbre.si@northeastern.edu>
2024-10-29 16:24:45 -0400

    route53,s3 and cloudwatch config

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "my_ami_ec2" {
     delete_on_termination = true
   }
   disable_api_termination = false
+  iam_instance_profile    = aws_iam_instance_profile.ec2_instance_profile.name
 
   user_data = <<-EOF
               #!/bin/bash
@@ -24,6 +25,14 @@ resource "aws_instance" "my_ami_ec2" {
 
               sudo mv /home/ec2-user/.env /usr/bin/.env
               sudo systemctl restart webapp.service
+
+              # Configure CloudWatch
+              sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+              -a fetch-config \
+              -m ec2 \
+              -c file:/opt/cloudwatch-config.json \
+              -s
+              
               EOF
 }
 
@@ -41,6 +50,79 @@ resource "aws_db_instance" "my_rds_instance" {
   password               = var.DB_Password
   parameter_group_name   = aws_db_parameter_group.rds_parameter_group.name
   skip_final_snapshot    = var.skip_final_snapshot
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-s3-access-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.cloudwatch_access_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-s3-access-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "s3-access-policy"
+  description = "Policy to allow EC2 instances access to the specific S3 bucket"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::sidd1234",
+          "arn:aws:s3:::sidd1234/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "cloudwatch_access_policy" {
+  name        = "CloudWatchAccessPolicy"
+  description = "Policy to allow CloudWatch access"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:PutMetricData",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:CreateLogGroup"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_vpc" "main" {
